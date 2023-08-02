a2da98ce54eee13a56f18094d568a195ac7d83c1
ThisIsFrankTsai <vg8rwb@gmail.com>
2023-08-02 21:00:05 +0800

    create Products

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from mysite.models import NewTable,Product
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin) :
+    list_display=('name','price','qty')
+
+
+admin.site.register(NewTable)
+admin.site.register(Product,ProductAdmin)
